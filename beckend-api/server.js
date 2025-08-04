abeda150ae0e6dede3716290d04794ac48b5e923
Cristian Buonocore <cristian.buonocore680@gmail.com>
2025-08-04 14:32:51 +0200

    Edit import layout to connect backend with front-end added images in public

--- beckend-api/server.js
+++ beckend-api/server.js
@@ -2,7 +2,17 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT
 const movieRouter = require('./routers/movies')
+const cors = require("cors");
 
+app.use(express.static('public'))
+
+// consenti l'accesso solo al sito che ha questo indirizzo
+app.use(cors({
+    origin: "http://localhost:5173"
+}));
+
+// consente l'accesso a qualsiasi sito in qualsiasi momento
+// app.use(cors());
 
 app.use('/api/movies', movieRouter)
 
